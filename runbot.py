cb656ce0196fda704b4ff9226bc31fd89c35467f
Venkateshwar Yadav (McCharlie Sins) <vkyd1909@gmail.com>
2023-05-30 13:46:42 +0530

    testing a new section

--- runbot.py
+++ runbot.py
@@ -2,42 +2,43 @@ import random
 import time
 import pyautogui
 
-comments = [
-    "Awesome post! 😄",
-    "Thanks for sharing! 🙌",
-    "Love this! ❤️",
-    "Great job, well done! 👍",
-    "Keep up the good work! 👏",
-    "Haha, that's hilarious! 😂",
-    "I totally agree! 👌",
-    "You're amazing! 😊",
-    "Fantastic content! 🌟",
-    "Woohoo! Congrats! 🎉",
-    "This made my day! 😃",
-    "Thank you so much! 🙏",
-    "Brilliant post, loved it! 💯",
-    "You're a rockstar! 🤘",
-    "So inspiring! 💪",
-    "This is pure gold! 🏆",
-    "Absolutely stunning! 😍",
-    "Made me smile! 😄",
-    "Well said! 👏👏👏",
-    "Impressive work! 👌👍",
-    "You're on fire! 🔥",
-    "I'm speechless! 😶",
-    "Mind-blowing content! 🤯",
-    "This is genius! 🧠",
-    "Keep shining bright! ✨",
-    "You're a superstar! ⭐",
-    "Absolutely flawless! 💎",
-    "This is everything! 🙌💯",
-    "Hats off to you! 👒👏",
-    "This deserves applause! 👏👏👏",
-    "You're an inspiration! 💫"
-]
+# comments = [
+#     "Awesome post! 😄",
+#     "Thanks for sharing! 🙌",
+#     "Love this! ❤️",
+#     "Great job, well done! 👍",
+#     "Keep up the good work! 👏",
+#     "Haha, that's hilarious! 😂",
+#     "I totally agree! 👌",
+#     "You're amazing! 😊",
+#     "Fantastic content! 🌟",
+#     "Woohoo! Congrats! 🎉",
+#     "This made my day! 😃",
+#     "Thank you so much! 🙏",
+#     "Brilliant post, loved it! 💯",
+#     "You're a rockstar! 🤘",
+#     "So inspiring! 💪",
+#     "This is pure gold! 🏆",
+#     "Absolutely stunning! 😍",
+#     "Made me smile! 😄",
+#     "Well said! 👏👏👏",
+#     "Impressive work! 👌👍",
+#     "You're on fire! 🔥",
+#     "I'm speechless! 😶",
+#     "Mind-blowing content! 🤯",
+#     "This is genius! 🧠",
+#     "Keep shining bright! ✨",
+#     "You're a superstar! ⭐",
+#     "Absolutely flawless! 💎",
+#     "This is everything! 🙌💯",
+#     "Hats off to you! 👒👏",
+#     "This deserves applause! 👏👏👏",
+#     "You're an inspiration! 💫"
+# ]
 
-waittime=range(1,8)
-while True:
-    random_comment = random.choice(comments)
-    random_wait=random.choice(waittime)
-    print(random_wait,random_comment)
+# waittime=range(1,8)
+# while True:
+#     random_comment = random.choice(comments)
+#     random_wait=random.choice(waittime)
+#     print(random_wait,random_comment)
+while
